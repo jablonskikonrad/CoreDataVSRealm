97bd687c53ae2a1cb27172b55e2b8d4547124535
Konrad Jabłoński <konrad.jablonski@onvil.tech>
2024-06-02 01:06:58 +0200

    Initial Commit

---
+++ DatabasesPerformance/DatabasesPerformanceApp.swift
@@ -0,0 +1,20 @@
+//
+//  DatabasesPerformanceApp.swift
+//  DatabasesPerformance
+//
+//  Created by konradjablonskipriv on 02/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct DatabasesPerformanceApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
